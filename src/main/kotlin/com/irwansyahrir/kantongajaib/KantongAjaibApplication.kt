0fc56fc0f3754fe53391c687af3a27bb5a250cf0
irwansyahrir <irwan.syahrir@gmail.com>
2019-05-29 17:24:46 +0200

    First commit

---
+++ src/main/kotlin/com/irwansyahrir/kantongajaib/KantongAjaibApplication.kt
@@ -0,0 +1,11 @@
+package com.irwansyahrir.kantongajaib
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KantongAjaibApplication
+
+fun main(args: Array<String>) {
+	runApplication<KantongAjaibApplication>(*args)
+}
